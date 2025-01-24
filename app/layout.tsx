951c2a3aaf2448224076402e554bfba2a13f0f31
shivam <shivamt0407@gmail.com>
2025-01-24 20:46:19 +0530

    feat:add toast component in the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${font.className} ${font.style} antialiased`}>
+          <Toaster />
           {children}
         </body>
       </html>
